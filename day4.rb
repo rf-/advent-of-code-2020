cbe8835498ff211411f7fac9a343283a1b2b5cf3
Ryan Fitzgerald <rwfitzge@gmail.com>
2020-12-03 21:42:55 -0800

    Day 4

---
+++ day4.rb
@@ -0,0 +1,32 @@
+require 'active_support/all'
+require 'pp'
+require 'pry'
+require 'set'
+
+INPUT = File.read('day4.input').chomp
+
+passports = INPUT.split(/\n\n/).map do |stuff|
+  stuff.scan(/(\w{3}):(.*?)(?: |\n|$)/).to_h
+end
+
+# Part 1
+
+result_1 = passports.count do |pp|
+  (%w[byr iyr eyr hgt hcl ecl pid] - pp.keys).empty?
+end
+
+puts result_1 # 204
+
+# Part 2
+
+result_2 = passports.count do |pp|
+  pp['byr'] =~ /^\d{4}$/ && (1920..2002).cover?($&.to_i) &&
+  pp['iyr'] =~ /^\d{4}$/ && (2010..2020).cover?($&.to_i) &&
+  pp['eyr'] =~ /^\d{4}$/ && (2020..2030).cover?($&.to_i) &&
+  pp['hgt'] =~ /^(\d+)(cm|in)$/ && ($2 == 'cm' ? 150..193 : 59..76).cover?($1.to_i) &&
+  pp['hcl'] =~ /^#\h{6}$/ &&
+  pp['ecl'] =~ /^(amb|blu|brn|gry|grn|hzl|oth)$/ &&
+  pp['pid'] =~ /^\d{9}$/
+end
+
+puts result_2 # 179
