dc38b52955efb9200b78efde15004409af71af87
Ryan Fitzgerald <rwfitzge@gmail.com>
2020-12-06 23:06:30 -0800

    Day 7

---
+++ day7.rb
@@ -0,0 +1,57 @@
+require 'active_support/all'
+require 'pp'
+require 'pry'
+require 'set'
+
+INPUT = File.readlines('day7.input').map(&:chomp)
+
+rules = {}
+INPUT.each do |line|
+  container_color = line[/^(.*?) bag/, 1]
+  contained = line.scan(/(\d+) (.+?) bag/).map { [_1.to_i, _2] }
+  rules[container_color] = contained
+end
+
+rules_inverted = Hash.new { _1[_2] = [] }
+rules.each do |container_color, contained|
+  contained.each do |count, contained_color|
+    rules_inverted[contained_color].push([count, container_color])
+  end
+end
+
+# Part 1
+
+def count_containing_colors(rules_inverted, initial_color)
+  seen = Set.new
+  pending = Set.new([initial_color])
+
+  while pending.any?
+    current_color = pending.to_a[0]
+    pending.delete(current_color)
+    seen.add(current_color)
+
+    rules_inverted[current_color].each do |count, color|
+      pending.add(color) unless seen.include?(color)
+    end
+  end
+
+  seen.count - 1 # remove original color
+end
+
+result_1 = count_containing_colors(rules_inverted, 'shiny gold')
+
+puts result_1 # 316
+
+# Part 2
+
+def count_contained_bags(rules, color)
+  return 0 if rules[color].nil?
+
+  rules[color].sum do |count, contained_color|
+    (1 + count_contained_bags(rules, contained_color)) * count
+  end
+end
+
+result_2 = count_contained_bags(rules, 'shiny gold')
+
+puts result_2 # 11310
--- template.rb
+++ template.rb
@@ -7,15 +7,6 @@ INPUT = File.read('dayN.input').chomp
 # or
 INPUT = File.readlines('dayN.input').map(&:chomp)
 
-# require './lib/grid'
-# grid = Grid.new(:y_down OR :y_up)
-# grid.fill(INPUT[0].length, INPUT.length, INPUT.flat_map { |l| l.split('') })
-# grid.bearing(pt1, pt2)
-# grid.distance(pt1, pt2)
-
-# require './lib/graph_utils'
-# GraphUtils.topo_sort([[dependency, dependent], ...])
-
 # Part 1
 
 result_1 = nil
@@ -29,3 +20,13 @@ result_2 = nil
 puts result_2
 
 Pry.start(binding, quiet: true)
+
+# require './lib/grid'
+# grid = Grid.new(:y_down OR :y_up)
+# grid.fill(INPUT[0].length, INPUT.length, INPUT.flat_map { |l| l.split('') })
+# grid.bearing(pt1, pt2)
+# grid.distance(pt1, pt2)
+
+# require './lib/graph_utils'
+# GraphUtils.topo_sort([[dependency, dependent], ...])
+
